b5829dd8f1ba3c304a20d0a9ade49bbfb950c936
Nikhilkps1904 <nikhilkps1538@gmail.com>
2025-01-11 23:35:00 +0530

    inital

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,22 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './utils/db.js';
+import userRouter from './routes/user.route.js';
+
+
+
+
 dotenv.config();
 const app = express();
 
+app.use('/api/users', userRouter);
+// app.use('/api/products', productRouter);
+// app.use('/api/orders', orderRouter);
+// app.use('/api/upload', uploadRouter);
+// app.use('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
+// app.get('/', (req, res) => { 
+//     res.send('Server is ready'); 
+// });
 
 
 //midlewars
